a26d1525e8b1432ee4d9a47a49800f0189a40716
Yashna Meher <yashnamb@bu.edu>
2025-10-21 08:03:35 -0400

    Delete streamlit/utils directory

--- streamlit/utils/charts.py
+++
@@ -1,6 +0,0 @@
-import plotly.express as px
-
-def line_chart(df, x, y, title):
-    fig = px.line(df, x=x, y=y, title=title)
-    fig.update_layout(height=400, margin=dict(l=20,r=20,t=40,b=20))
-    return fig
